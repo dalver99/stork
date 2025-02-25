48c2fe0779541dd418107ea1423f6e65fbf425f9
Girae <gitae990723@gmail.com>
2025-02-25 11:58:52 +0900

    Initial commit

---
+++ next.config.mjs
@@ -0,0 +1,14 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+    async rewrites() {
+      return [
+        {
+          source: '/api/:path*',
+          destination: 'http://localhost:5328/api/:path*',
+        },
+      ];
+    },
+  };
+  
+  export default nextConfig;
+  
